1d7e510f826464a34b1a0e8525bd462d99152a84
zihad868 <zihadh868@gmail.com>
2023-12-05 23:21:06 +0600

    Swagger Api Documentation

--- ecomApi/ecomApi/urls.py
+++ ecomApi/ecomApi/urls.py
@@ -17,8 +17,29 @@ from django.contrib import admin
 from rest_framework.authtoken import views
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Django Rest Framowork",
+      default_version='v1',
+      description="Ecommarce API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('api-token-auth/', views.obtain_auth_token),
-    path('', include('product.urls'))
+    path('call-api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('list-api/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include('product.urls')),
 ]
--- ecomApi/product/serializers.py
+++ ecomApi/product/serializers.py
@@ -1,53 +1,58 @@
 from rest_framework import serializers
 from django.utils.text import slugify
 
-from . models import Product, Category
+from .models import Product, Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
-    
-    
+
     def to_representation(self, instance):
         context = super().to_representation(instance)
-        context['category'] = {'id': instance.category.id, 'title': instance.category.title, 'slug': instance.category.slug}
+        context['category'] = {'id': instance.category.id, 'title': instance.category.title,
+                               'slug': instance.category.slug}
         return context
-    
+
     def update(self, instance, validated_data):
         context = super().update(instance, validated_data)
         context.slug = slugify(context.title)
         context.save()
         return context
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
-    
+
 
 class ProductRetriveSerializer(serializers.ModelSerializer):
     related_products = serializers.SerializerMethodField(method_name='get_related_product')
+
     class Meta:
         model = Product
-        fields = ['id', 'category', 'title', 'slug', 'featured', 'thumbnail', 'price', 'description', 'related_products']
-        
+        fields = ['id', 'category', 'title', 'slug', 'featured', 'thumbnail', 'price', 'description',
+                  'related_products']
+
     def to_representation(self, instance):
         context = super().to_representation(instance)
-        
-        context['category'] = {'id': instance.category.id, 'title': instance.category.title, 'slug': instance.category.slug}
+
+        context['category'] = {'id': instance.category.id, 'title': instance.category.title,
+                               'slug': instance.category.slug}
         return context
-    
+
     def get_related_product(self, obj):
         return ProductSerializer(obj.related, many=True).data
-    
+
 
 class CategoryRetriveSerializer(serializers.ModelSerializer):
     categories = serializers.SerializerMethodField(method_name='get_category')
+
     class Meta:
         model = Category
         fields = ['id', 'title', 'slug', 'featured', 'categories', 'created_date']
-    
+
     def get_category(self, obj):
         return ProductSerializer(obj.products.all(), many=True).data
